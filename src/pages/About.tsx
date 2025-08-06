160905fb3b36ef286f1ed43d0b5536b5110fe24d
Bjørn Ravlo-Leira <bjornrleira@Bjrns-MacBook-Pro.local>
2025-08-06 14:08:46 +0200

    minor changes and bug fixes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -180,7 +180,7 @@ export default function About() {
             <div className="-mt-12 -mr-12 p-12 lg:sticky lg:top-4 lg:col-start-1 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
               <img
                 alt=""
-                src="https://tailwindcss.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
+                src="/retusjering-eksempel.jpg"
                 className=" rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-228"
               />
             </div>
